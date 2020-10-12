93d9729f74f701c294e70111fd28aa180dd1c4ba
thanush96 <danushthanush@gmail.com>
2020-10-12 21:25:50 +0530

    Step : 8 Show Hello World message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+        Hello World
     </div>
   );
 }
