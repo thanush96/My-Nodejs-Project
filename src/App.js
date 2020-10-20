ec120164219fbda84b66d5fd46f8d56b2e068607
thanush96 <danushthanush@gmail.com>
2020-10-20 13:28:47 +0530

    Step 11 : Create Exercise Log component Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import {BrowserRouter as Router,Route} from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import NavBar from "./components/navbar.component";
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
-
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
 
 function App() {
-  return (
-    <Router>
-      <NavBar/>
-      <br/>
-      <Route path="/" exact component={ExercisesList}/>
-      <Route path="/edit/:id" exact component={EditExercise}/>
-      <Route path="/create" exact component={CreateExercise}/>
-      <Route path="/user" exact component={CreateUser}/>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="container">
+				<Navbar />
+				<br />
+				<Route path="/create" component={CreateExercise} />
+				
+			</div>
+		</Router>
+	);
 }
 
 export default App;
