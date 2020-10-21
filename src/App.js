ef98172a6284904a293a397fdf1061a01e4cac54
thanush96 <danushthanush@gmail.com>
2020-10-21 14:11:56 +0530

    Step 16 : Finally Edit and Update Exercise Logs in list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import Navbar from './components/navbar.component';
 import ExercisesList from './components/exercises-list.component';
 import EditExercise from './components/edit-exercise.component';
@@ -17,7 +16,7 @@ function App() {
 				<Route path="/create" component={CreateExercise} />
 				<Route path="/user" component={CreateUser} />
 				<Route path="/" exact component={ExercisesList} />
-				
+				<Route path="/edit/:id" component={EditExercise} />
 			</div>
 		</Router>
 	);
